29f6054b88a4eae795a15f6f241fd672db95624d
bedwardsphoto <brian@brianedwardsphotography.com>
2016-03-29 13:58:09 -0400

    Done.

--- db/migrate/01_create_songs.rb
+++ db/migrate/01_create_songs.rb
@@ -1,2 +1,9 @@
 class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |a|
+      a.string :name
+      a.integer :artist_id
+      a.integer :genre_id
+    end
+  end
 end
