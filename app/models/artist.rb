9983c24163e53841c5ca24346d1d4ef06fac488a
motti770 <mottigottlieb@gmail.com>
2016-05-17 17:30:46 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,16 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+    has_many :genres, through: :songs
 end
+
+# has_many is concidered the parent 
+    # telling a child who is parent is won't also let the parent know 
+    # it has a new child
+        #         # however telling a parent that he got a new child both will know
+        #         [10] pry(main)> rolling_in_the_deep = Song.new(name: "Rolling in the Deep")
+        # => #<Song:0x007fc75bb4d1e0 id: nil, name: "Rolling in the Deep", artist_id: nil, genre_id: nil>
+        # [11] pry(main)> adele.songs << rolling_in_the_deep
+        # => [ #<Song:0x007fc75bb4d1e0 id: nil, name: "Rolling in the Deep", artist_id: nil, genre_id: nil>]
+        # [12] pry(main)> rolling_in_the_deep.artist
+        # => #<Artist:0x007fc75b8d9490 id: nil, name: "Adele">
+        
--- db/migrate/01_create_songs.rb
+++ db/migrate/01_create_songs.rb
@@ -1,2 +1,21 @@
 class CreateSongs < ActiveRecord::Migration
+    
+    #migration to create table songs 
+    #where a song has a name and joins artist_id and genre_id via joins implementing a many to many relationship
+    
+        
+        # id | name           |  artist_id    |     genre_id  |
+        # ----------------------------------------------------
+        # 2  | Shake it Off   | 1             |    1          |
+        # ----------------------------------------------------
+         
+    def change
+       create_table :songs do |t|
+           t.string :name
+           t.integer :artist_id
+           t.integer :genre_id 
+    end
+    
+end
+
 end
