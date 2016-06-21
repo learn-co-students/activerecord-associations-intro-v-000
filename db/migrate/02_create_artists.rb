6e8e7677f1a18bdfb3621a8a005fb1177831b222
kaileeagray <kaileeagray@gmail.com>
2016-06-21 13:01:47 -0500

    Done.

--- db/migrate/02_create_artists.rb
+++ db/migrate/02_create_artists.rb
@@ -1,2 +1,9 @@
+#artist will have many songs and it will have many genres through songs
+
 class CreateArtists < ActiveRecord::Migration
+  def change
+    create_table :artists do |t|
+      t.string :name
+    end
+  end
 end
