71c1968229b79f74ea221b49a0049dc4c4149690
Joseph Hyatt <jhyatt1017@gmail.com>
2017-05-27 15:54:08 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,10 @@
+#can build a song
+#can create a song
+#knows about songs that are affiliated with it
+#can add many songs at the same time
+#knows about its genres 
+
 class Artist < ActiveRecord::Base
+  has_many :songs
+  has_many :genres, through: :songs
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,7 @@
+#has many songs
+#is also associated with an artist 
+
 class Genre < ActiveRecord::Base
+  has_many :songs
+  has_many :artists, through: :songs
 end
