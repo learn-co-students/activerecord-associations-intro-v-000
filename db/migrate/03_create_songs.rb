f424ed2faa4cad5fb3c7776e41f3fd027e56175f
Richard Burd <richardburdx@yahoo.com>
2017-10-19 23:03:54 +0000

    Done.

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,9 @@
 class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |table|
+      table.string :name
+      table.integer :artist_id
+      table.integer :genre_id
+    end
+  end
 end
