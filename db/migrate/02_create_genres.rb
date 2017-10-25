6972ee193b9e78e401ba29d300f4766479000595
Nicholas Halloran <nmhalloran@hotmail.com>
2017-10-25 18:30:40 +0000

    Done.

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,2 +1,7 @@
-class CreateGenres < ActiveRecord::Migration
+class CreateGenres < ActiveRecord::Migration[4.2]
+  def change
+    create_table :genres do |genre|
+      genre.string :name
+    end
+  end
 end
