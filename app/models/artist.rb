9dc18c9fa1212ada5e9dac6e2f70538c18b94e17
Alex <github email address>
2017-11-07 12:08:46 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,3 +2,6 @@ class Artist < ActiveRecord::Base
     has_many :songs
     has_many :genres, through: :songs
 end
+
+
+#When a model has_many of something, it will store those objects in an array. o add to that collection, we use the shovel operator, <<, to operate on that collection, treat adele.songs like any other array.
