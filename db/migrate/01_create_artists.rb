19ed6faa28943ae59561baf8a220b4f43a33f5db
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-08 21:39:08 +0000

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,2 +1,9 @@
-class CreateArtists < ActiveRecord::Migration
+class CreateArtists < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.timestamp
+    end
+  end
 end
