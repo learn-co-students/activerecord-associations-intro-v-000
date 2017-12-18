bd36b16c0dfbbaa0307fe48ac335e89e1e7a90af
Anastasia  Barkett <anastasia.barkett@gmail.com>
2017-12-18 03:23:33 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 4) do
   create_table "songs", force: :cascade do |t|
     t.string  "name"
     t.integer "artist_id"
-    t.integer "genere_id"
+    t.integer "genre_id"
   end
 
 end
