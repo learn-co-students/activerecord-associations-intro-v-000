3a7f7b489cf9277aafe7040d706aed2d09e8d24a
ruthmesfun <ruth.mesfun@gmail.com>
2018-02-16 06:54:11 -0500

    Done.

--- db/migrate/04_add_artist_to_songs.rb
+++ db/migrate/04_add_artist_to_songs.rb
@@ -1,2 +1,8 @@
 class AddArtistToSongs < ActiveRecord::Migration
+  def change
+    change_table :songs do |t|
+      t.belongs_to :artist, index: true
+      t.belongs_to :genre, index: true
+    end
+  end
 end
