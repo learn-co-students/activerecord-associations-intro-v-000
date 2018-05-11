b29257d58022f2f0b7d663408e272780a6013c1d
Jayon Kim <jpkim921@gmail.com>
2018-05-11 02:19:45 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,5 @@
 class Song < ActiveRecord::Base
-
+# We need to tell the `Song` class that it will produce objects that can belong to an artist. We will do it with the `belongs_to` macro:
+  belongs_to :artist
+  belongs_to :genre
 end
