d77a4a31b1d4cf6718cb3b00888cd5815a071a05
Christine Cua <cloudycua@gmail.com>
2018-06-08 16:04:28 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,6 @@
 class Artist < ActiveRecord::Base
+  # tells the Artist class that each artist object can have many songs
+  has_many :songs
+  # tells the Artist class that each artist can have many genres through songs
+  has_many :genres, through: :songs
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,5 @@
 class Song < ActiveRecord::Base
-
+  # tells the Song class that it will produce objects that can belong to an artist or genre
+  belongs_to :artist
+  belongs_to :genre
 end
