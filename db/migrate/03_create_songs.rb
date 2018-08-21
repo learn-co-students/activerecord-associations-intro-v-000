2e13b03a5134a895f583b4ba2c58f935642c65fd
Madeline Stark <madeline.stark1@gmail.com>
2018-08-21 16:23:04 +0000

    Done.

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,10 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.integer :artist_id
+      t.integer :genre_id
+    end
+  end
 end
+#songs is functioning as the join table with an artist id and a genre id 
