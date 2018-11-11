8afd508f71ec98ecbcfcef900bdcc977b97b01d7
Mohammed Noori <kmanoorib@gmail.com>
2018-11-11 02:29:55 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,8 @@
 class Artist < ActiveRecord::Base
+  
+  has_many :songs
+  
+  # has_many through
+  has_many :genres, through: :songs
+  
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,8 @@
 class Genre < ActiveRecord::Base
+  
+  has_many :songs
+  
+  # has_many through
+  has_many :artists, through: :songs
+  
 end
