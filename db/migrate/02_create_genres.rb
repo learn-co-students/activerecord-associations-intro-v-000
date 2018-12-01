e5ce7830284eea997d468142de4c51fade474176
Rachel Wyatt <rw126@evansville.edu>
2018-12-01 22:05:25 +0000

    Automatically backed up by Learn

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,7 +1,9 @@
-class CreateGenres < ActiveRecord::Migration[4.2]
+  class CreateGenres < ActiveRecord::Migration[4.2]
   def change
     create_table :genres do |t|
       t.string :name
+      t.string :other
+      #comment
     end
   end
-end
+  end
