9044f5bc62b2c11dcc964bb87b0a30df9657a021
Sharmaine Requerme <srqrme@gmail.com>
2018-12-05 22:27:35 +0000

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -3,5 +3,9 @@ class CreateArtists < ActiveRecord::Migration[4.2]
     create_table :artists do |t|
       t.string :name
     end
+
+    create_table :songs do |t|
+      t.belongs_to :artist,
+    end
   end
 end
