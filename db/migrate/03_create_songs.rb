22a9a095e3ae7e4fbf0ea086fd9faeac76c4b39b
Ryan Brady <ryanbrady369@gmail.com>
2019-01-30 22:32:01 +0000

    Done.

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,9 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+  def change
+    create_table :songs do |q|
+      q.string :name
+      q.integer :artist_id
+      q.integer :genre_id
+    end
+  end
 end
