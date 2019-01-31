549631b4ddbd2ff4ca053f75719a0a2e770a5fbe
Ashley Wong <ashwong04@gmail.com>
2019-01-31 14:00:46 +0000

    Done.

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,11 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+  
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.integer :atrist_id
+      t.integer :gengre_id
+    end
+  end
+  
 end
