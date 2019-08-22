c14e6446a5e713636a67e78a05399951314be28a
Tim Knoll <timothy.knoll@gmail.com>
2019-08-22 02:16:30 +0000

    Done.

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,2 +1,5 @@
 class CreateGenres < ActiveRecord::Migration[4.2]
+
 end
+
+# genre will have many songs and it will have many artists through songs
--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,9 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+# we will give a given song an artist_id value of the artist it belongs to
+# we will give a given song an genre_id value of the genre it belongs to
+
+
+ 
 end
+
+# song will belong to and ARTIST and belong to a GENRE
