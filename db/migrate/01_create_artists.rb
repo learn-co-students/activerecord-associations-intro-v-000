6ff38ca8dbd5477811af0e2f3ddf40c3d33c98a9
Tim Knoll <timothy.knoll@gmail.com>
2019-08-22 03:57:48 +0000

    Done.

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,5 +1,9 @@
 class CreateArtists < ActiveRecord::Migration[4.2]
- 
+  def change
+    create_table :artists do |t|
+      t.string :name
+    end
+  end 
 end
 
 # artist will have many songs and it will have many genres through songs
